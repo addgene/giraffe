b89b6736ab6bea67b1c6a46d24a20f4a3acc5f2e
Benjie Chen <benjie@addgene.org>
2011-02-15 12:37:42 -0500

    cleanup different FPs: we now detect the base FPs using DNA, then have protein mutations detected for variants

--- src/django/giraffe/blat/tags.py
+++ src/django/giraffe/blat/tags.py
@@ -15,5 +15,25 @@ PROTEIN_TAGS = (
     ("Strep Tag","WSHPQFEK"),
     ("SBP Tag","MDEKTTGWRGGHVVEGLAGELEQLRARLEHHPQGQREP"),
     ("TAP","GRRIPGLINPWKRRWKKNFIAVSAANRFKKISSSGALDYDIPTTASENLYFQGEFGLAQHDEAVDNKFNKEQQNAFYEILHLPNLNEEQRNAFIQSLKDDPSQSANLLAEAKKLNDAQAPKVDNKFNKEQQNAFYEILHLPNLNEEQRNAFIQSLKDDPSQSANLLAEAKKLNDAQAPKVDANHQ"),
+
+    ("EGFP (Y66)", "VTTLTYGVQC"),
+    ("ECFP (Y66W)","VTTLTWGVQC"),
+    ("EBFP (Y66H)","VTTLTHGVQC"),
+    ("EYFP (T203Y)","DNHYLSYQSALSK"),
+    ("Cerulean (Y145A,H148D)","LEYNANSDNV"),
+    ("Venus (M153T,V163A,S175G)","YITADKQKNGIKANFKIRHNIEDGGVQL"),
+
+    # removed start codon M; may not be there for fusion protein
+    ("mRFP/Tan/Hon 5' terminal","ASSEDVIK"),
+    ("mCherry 5' terminal",     "VSKGEEDNMA"),
+    ("GFP 5' terminal",         "VSKGEENNMA"),
+    ("GFP 3' terminal",         "GMDELYK"),
+    ("mCherry (Q66M)",              "FAWDILSPQFMYGSKAYV"),
+    ("mStrawberry (S62T,Q64N,Q66T)","FAWDILTPNFTYGSKAYV"),
+    ("mOrange (Q66T)",              "FAWDILSPQFTYGSKAYV"),
+    ("mTan/Ban (Q66C)",             "FAWDILSPQFCYGSKAYV"),
+    ("mHoneydew (Y67W)",            "FAWDILSPQFMWGSKAYV"),
+    ("mBanana (I197E)",             "YIAGEKIDI"),
+    ("mTangerine (I197)",           "YKTDIKLDI"),
 )
 
