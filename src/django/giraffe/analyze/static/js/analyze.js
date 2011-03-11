83aab38a6a62f7fe878dfce7b710600a3ae9cebe
Benjie Chen <benjie@addgene.org>
2011-03-11 12:21:07 -0500

    more analyzer stuff

--- src/django/giraffe/analyze/static/js/analyze.js
+++ src/django/giraffe/analyze/static/js/analyze.js
@@ -14,61 +14,64 @@
     if ('dom_id' in options) { dom_id = options['dom_id']; }
     var name = 'Sequence';
     if ('name' in options) { name = options['name']; }
+    var sequence = new BioJS.DNASequence(gd.sequence);
 
-    function sequence_tab(dom) {
-        var viewer = $('<div></div>');
-        var fasta = $('<div></div>');
-        var genbank = $('<div></div>');
-        var rc = $('<div></div>');
-
-        var link_viewer = $('<a href="#">Viewer</a>').click(function(){
-            hide_all();
-            $(this).addClass('giraffe-link-on');
-            $(viewer).show();
-        });
-        var link_fasta = $('<a href="#">FASTA</a>').click(function(){
-            hide_all();
-            $(this).addClass('giraffe-link-on');
-            $(fasta).show();
-        });
-        var link_genbank = $('<a href="#">GenBank</a>').click(function(){
-            hide_all();
-            $(this).addClass('giraffe-link-on');
-            $(genbank).show();
-        });
-        var link_rc = $('<a href="#">Reverse Complement</a>').click(function(){
-            hide_all();
-            $(this).addClass('giraffe-link-on');
-            $(rc).show();
-        });
-
-        var all_divs = [viewer,fasta,genbank,rc];
-        var all_links = [link_viewer,link_fasta,link_genbank,link_rc];
+    function Switch_Panes(panes) {
+        var divs = [];
+        var links = [];
 
         function hide_all() {
-            for (var i in all_divs) { $(all_divs[i]).hide(); } 
-            for (var i in all_links) { $(all_links[i]).removeClass('giraffe-link-on'); } 
+            for (var i in divs) { $(divs[i]).hide(); } 
+            for (var i in links) { $(links[i]).removeClass('giraffe-link-on'); } 
+        }
+        function show(i) {
+            hide_all();
+            $(links[i]).addClass('giraffe-link-on');
+            $(divs[i]).show();
+        }
+        function link(i) { return links[i]; }
+        function pane(i) { return divs[i]; }
+
+        var links_dom = $('<p></p>');
+        var divs_dom = $('<div></div>');
+
+        for (var i in panes) {
+            divs[i] = $('<div></div>');
+            links[i] = $('<a pane="'+i+'" href="#">'+panes[i]+'</a>').click(function() {
+                var i = $(this).attr('pane'); show(i);
+            });
+            $(links_dom).append(links[i]);
+            if (i < panes.length-1) { $(links_dom).append(' | '); }
+            $(divs_dom).append(divs[i]);
         }
 
-        var links = $('<p></p>')
-            .append(link_viewer).append(' | ')
-            .append(link_fasta).append(' | ')
-            .append(link_genbank).append(' | ')
-            .append(link_rc);
+        return {
+            'links' : links_dom,
+            'panes' : divs_dom,
+            'pane' : pane,
+            'link' : link,
+            'show' : show,
+            'hide_all' : hide_all,
+        }
+    }
 
-        $(dom)
-            .append('<p>Current sequence: '+gd.sequence.length+' base pairs</p>')
-            .append(links)
-            .append(viewer)
-            .append(fasta)
-            .append(genbank)
-            .append(rc);
+    function sequence_tab(dom) {
+        panes = Switch_Panes(['Viewer', 'Fasta', 'GenBank', 'Reverse Complement']);
 
-        $(fasta).addClass('giraffe-seq').append(BioJS.fasta_html(gd.sequence,name));
+        $(dom).append('<p>Current sequence: '+sequence.length()+' base pairs</p>')
+              .append(panes.links)
+              .append(panes.panes);
 
-        hide_all();
-        $(link_fasta).addClass('giraffe-link-on');
-        $(fasta).show();
+        $(panes.pane(1))
+            .addClass('giraffe-seq').append(BioJS.fasta(sequence,name));
+        $(panes.pane(2))
+            .addClass('giraffe-seq').append(BioJS.genbank(sequence,name));
+        $(panes.pane(3))
+            .addClass('giraffe-seq').append(
+                BioJS.fasta(sequence.reverse_complement(),name)
+            );
+        panes.hide_all();
+        panes.show(1);
     }
 
     function map_tab(dom) {
@@ -81,6 +84,40 @@
             'fade_time' : 300,
             'cutters': [1]
         });
+    }
+
+    function translate_tab(dom) {
+        panes = Switch_Panes(
+            ['ORFs',
+             "5'3' Frame 1",
+             "5'3' Frame 2",
+             "5'3' Frame 3",
+             "3'5' Frame 1",
+             "3'5' Frame 2",
+             "3'5' Frame 3"]
+        );
+
+        $(dom).append(panes.links)
+              .append(panes.panes);
+
+        $(panes.pane(1))
+            .addClass('giraffe-seq').append(sequence.translate().format_html());
+        $(panes.pane(2))
+            .addClass('giraffe-seq').append(sequence.substring(1).translate().format_html());
+        $(panes.pane(3))
+            .addClass('giraffe-seq').append(sequence.substring(2).translate().format_html());
+        $(panes.pane(4))
+            .addClass('giraffe-seq').append(
+                sequence.reverse_complement().translate().format_html());
+        $(panes.pane(5))
+            .addClass('giraffe-seq').append(
+                sequence.reverse_complement().substring(1).translate().format_html());
+        $(panes.pane(6))
+            .addClass('giraffe-seq').append(
+                sequence.reverse_complement().substring(2).translate().format_html());
+
+        panes.hide_all();
+        panes.show(1);
     }
 
     function full_widget() {
@@ -119,6 +156,8 @@
 
         map_tab(dom_tab_map);
         sequence_tab(dom_tab_sequence);
+        translate_tab(dom_tab_translate);
+
     }
 
     full_widget();
--- src/django/giraffe/analyze/static/js/bio.js
+++ src/django/giraffe/analyze/static/js/bio.js
@@ -1,50 +1,217 @@
 
 window.BioJS = function(){
+    // Translation code was from 
+    // http://plindenbaum.blogspot.com/2010/12/server-side-javascript-translating-dna.html
 
-    // Returns reverse complement of sequence
-    function reverse_complement(seq) {
-    }
-
-    // Returns FASTA format of sequence
-    function fasta_html(seq,name) {
-        var lines_80 = wrap(seq,80);
-        var s = '<p>&gt;'+name+'<br/>';
-        for (var i=0; i<lines_80.length; i++) {
-            s += lines_80[i]+'<br/>';
+    function TranslationTable(name,ncbiString) {
+   	    this.name=name;
+   	    this.ncbiString=ncbiString;
+    };
+    TranslationTable.STANDARD=new TranslationTable(
+   	    "standard",
+   	    "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
+    );
+    TranslationTable.prototype.base2index=function(c) {
+   	    switch(c) {
+   		    case 'T':case 't': return 0;
+   		    case 'C':case 'c': return 1;
+   		    case 'A':case 'a': return 2;
+   		    case 'G':case 'g': return 3;
+   		    default: return -1;
         }
-        s += '</p>';
-        return s;
+    };
+    TranslationTable.prototype.translate=function(b1, b2, b3) {
+   	    if (arguments.length==1) {
+      	    var prot="";
+      	    for(var i=0; i+2<b1.length; i+=3) {
+         	    prot+= this.translate(b1[i],b1[i+1],b1[i+2]);
+            }
+      	    return prot;
+        }
+   	    var base1= this.base2index(b1);
+   	    var base2= this.base2index(b2);
+   	    var base3= this.base2index(b3);
+   	    if (base1==-1 || base2==-1 || base3==-1) { return '?'; }
+   	    else { return this.ncbiString[base1*16+base2*4+base3]; }
     }
 
-    // Returns GenBank format of sequence
-    function genbank_html(seq) {
+    function __repeat(s,n) {
+        var r=""; for (var a=0;a<n;a++) r+=s; return r;
     }
 
-    // Returns DNA to protein translation of sequence
-    function translate(seq) {
+    function __normalize(c) {
+        if (c == '*' ||
+            c == 'D' ||
+            c == 'H' ||
+            c == 'M' ||
+            c == 'N' ||
+            c == 'R' ||
+            c == 'V' ||
+            c == 'W') { return 'A'; }
+        if (c == 'B' ||
+            c == 'Y' ||
+            c == 'S') { return 'C'; }
+        if (c == 'K') { return 'G'; }
+        if (c == 'U') { return 'T'; }
+        if (c == 'd' ||
+            c == 'h' ||
+            c == 'm' ||
+            c == 'n' ||
+            c == 'r' ||
+            c == 'v' ||
+            c == 'w') { return 'a'; }
+        if (c == 'b' ||
+            c == 'y' ||
+            c == 's') { return 'c'; }
+        if (c == 'k') { return 'g'; }
+        if (c == 'u') { return 't'; }
+        return c;
     }
 
     // Returns array of sequence, split from input sequence
-    function wrap(seq,width) {
-        var res = [];
-        var line = '';
-        for (var i=0; i<seq.length; i++) {
-            line += seq[i];
-            if (line.length >= width){
-                res.push(line);
-                line = '';
+    function wrap(seq_string,width) {
+        if (seq_string) {
+            var res = [];
+            var line = '';
+            for (var i=0; i<seq_string.length; i++) {
+                line += seq_string[i];
+                if (line.length >= width){
+                    res.push(line);
+                    line = '';
+                }
             }
+            if (line.length > 0) { res.push(line); }
+            return res;
         }
-        if (line.length > 0) { res.push(line); }
-        return res;
+        return [];
+    }
+
+    function DNASequence(seq_string) { this.__sequence = seq_string; }
+
+    DNASequence.prototype.sequence=function() { return this.__sequence; }
+    DNASequence.prototype.length=function() { return this.__sequence.length; }
+
+    DNASequence.prototype.reverse_complement=function(){
+        if (this.__reverse_complement) { return this.__reverse_complement; }
+        var r = this.__sequence.split("").reverse();
+        var rc = r.map(function(c) {
+            c = __normalize(c);
+            if (c == 'a') { return 't'; }
+            if (c == 'A') { return 'T'; }
+            if (c == 'g') { return 'c'; }
+            if (c == 'G') { return 'C'; }
+            if (c == 't') { return 'a'; }
+            if (c == 'T') { return 'A'; }
+            if (c == 'c') { return 'g'; }
+            if (c == 'C') { return 'G'; }
+            return c;
+        });
+        rc = rc.join("");
+        this.__reverse_complement = new DNASequence(rc);
+        return this.__reverse_complement;
+    };
+
+    // Returns DNA to protein translation of sequence
+    DNASequence.prototype.translate=function(){
+        if (this.__translation) { return this.__translation; }
+        var p = TranslationTable.STANDARD.translate(this.__sequence);
+        this.__translation = new ProteinSequence(p);
+        return this.__translation;
+    }
+
+    // Returns substring
+    DNASequence.prototype.substring=function(i,j){
+        return new DNASequence(this.__sequence.substring(i,j));
+    }
+
+    // Format HTML 80 chars wide
+    DNASequence.prototype.format_html=function() {
+        if (this.__html) { return this.__html; }
+        var lines_80 = wrap(this.__sequence,80);
+        s = '';
+        for (var i=0; i<lines_80.length; i++) {
+            s += lines_80[i]+'<br/>';
+        }
+        this.__html = s;
+        return this.__html;
+    }
+
+    function ProteinSequence(seq_string) { this.__sequence = seq_string; }
+
+    ProteinSequence.prototype.sequence=function() { return this.__sequence; }
+    ProteinSequence.prototype.length=function() { return this.__sequence.length; }
+
+    // Format HTML 80 chars wide
+    ProteinSequence.prototype.format_html=function() {
+        if (this.__html) { return this.__html; }
+        var lines_80 = wrap(this.__sequence,80);
+        s = '';
+        for (var i=0; i<lines_80.length; i++) {
+            s += lines_80[i]+'<br/>';
+        }
+        this.__html = s;
+        return this.__html;
+    }
+
+    // Returns FASTA format of sequence
+    function fasta(seq_object,name,html) {
+        if (html === undefined) { html = true; }
+        var lines_80 = wrap(seq_object.sequence(),80);
+        var s = '';
+        if (html) { s = '<p>&gt;'+name+'<br/>'; }
+        else { s = '>'+name+'\n'; }
+        for (var i=0; i<lines_80.length; i++) {
+            s += lines_80[i];
+            if (html) { s += '<br/>'; } else { s += '\n'; }
+        }
+        if (html) { s += '</p>'; }
+        return s;
+    }
+
+    // Returns GenBank format of sequence. Currently does not handle
+    // feature list, we will work on that.
+    function genbank(seq_object,name,html) {
+        if (html === undefined) { html = true; }
+
+        var sp = "&nbsp;";
+        var delim = "<br/>";
+        var tab = sp+sp+sp+sp;
+        if (!html) { sp = " "; delim = "\n"; tab = "\t"; }
+
+        s =
+            'LOCUS'+__repeat(sp,7)+name+tab+seq_object.length+' bp '+tab+'DNA'+tab+'SYN'+delim+
+            'DEFINITION'+__repeat(sp,2)+name+delim+
+            'ACCESSION'+__repeat(sp,3)+delim+
+            'KEYWORDS'+__repeat(sp,4)+delim+
+            'SOURCE'+__repeat(sp,6)+delim+
+            __repeat(sp,2)+'ORGANISM'+__repeat(sp,2)+
+            'other sequences; artificial sequences; vectors.'+delim+
+            'ORIGIN'+delim;
+
+        var lines_10 = wrap(seq_object.sequence(),10);
+        for (var i=0,j=0; i<lines_10.length; i++) {
+            if (j == 0) {
+                var start = i*10+1;
+                if (start < 10) { s += __repeat(sp,4); }
+                else if (start < 100) { s += __repeat(sp,3); }
+                else if (start < 1000) { s += __repeat(sp,2); }
+                else if (start < 10000) { s += __repeat(sp,1); }
+                s += start;
+            }
+            s += sp+lines_10[i];
+            j++;
+            if (j == 6) { s += delim; j = 0; }
+        }
+        s += delim+'//'+delim;
+        return s;
     }
 
     return {
-        'reverse_complement' : reverse_complement,
-        'fasta_html' : fasta_html,
-        'genbank_html' : genbank_html,
-        'translate' : translate,
-        'wrap' : wrap
+        'fasta' : fasta,
+        'genbank' : genbank,
+        'wrap' : wrap,
+        'DNASequence' : DNASequence,
+        'ProteinSequence' : ProteinSequence
     }
 }();
 
