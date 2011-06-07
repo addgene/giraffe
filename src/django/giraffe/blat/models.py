f382f79f78c4b7bbaf00c6ce23641f92bed7ecb2
Mikhail Wolfson <wolfsonm@addgene.org>
2011-06-07 19:36:26 -0400

    Clean up sequence: remove all invalid characters

--- src/django/giraffe/blat/models.py
+++ src/django/giraffe/blat/models.py
@@ -40,13 +40,9 @@ class Giraffe_Mappable_Model(models.Model):
         import orfs
         db = Feature_Database.objects.get(name=db_name)
 
-        # remove leading > for FASTA sequence
-        if sequence.startswith('>'):
-            sequence = re.sub(r'^\>[^\n]*\n','',sequence);
-        # TODO Add more cleaning code here?
-
         # clean the sequence
         sequence = Sequence.strip(sequence)
+        sequence = re.sub(r'[^A-Za-z*]', '', sequence)
 
         # create sequence record
         s = Sequence()
