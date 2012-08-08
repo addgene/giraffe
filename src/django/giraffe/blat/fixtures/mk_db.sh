36ef243e100d90e00b8f2cb7f54da3ab14f6a9e4
Gallardo, Antonio <antonio.gallardo@quoininc.com>
2012-08-08 12:32:53 -0600

    Fluorescent proteins automatically recognize on our plasmid maps.

--- src/django/giraffe/blat/fixtures/mk_db.sh
+++ src/django/giraffe/blat/fixtures/mk_db.sh
@@ -3,6 +3,7 @@
 python import_features.py default < features/generic.features
 python import_features.py default < features/generic.primers
 python import_features.py default < features/tb.enzymes
+python import_features.py default < features/fp.features
 python create_frag_db.py default > ../frags/data/default.data
 
 python import_features.py afire < features/af.unc
