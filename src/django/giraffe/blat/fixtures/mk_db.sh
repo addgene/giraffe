dd45d8f9632b2538ea8f84d92cc5dbf0617ab5c4
Benjie Chen <benjie@carbon.addgene.org>
2012-11-01 15:05:18 -0400

    added import_feature.py management command to replace import_features.py and updated mk_db.sh

--- src/django/giraffe/blat/fixtures/mk_db.sh
+++ src/django/giraffe/blat/fixtures/mk_db.sh
@@ -1,18 +1,82 @@
 #!/bin/sh
 
-PYTHON='/srv/addgene/bin/python'
+## Use the -W ignore::UserWarning to hide
+## "The virtualenv distutils package at %s appears to be in the same location as the system distutils?")
+PYTHON="python -W ignore::UserWarning"
+hname=`hostname`
+if [ "$hname" = "carbon" ]
+then
+	PYTHON='/srv/addgene/bin/python -W ignore::UserWarning'
+fi
 
-$PYTHON import_features.py default < features/generic.features
-$PYTHON import_features.py default < features/generic.primers
-$PYTHON import_features.py default < features/tb.enzymes
-$PYTHON import_features.py default < features/fp.features
-$PYTHON create_frag_db.py default > ../frags/data/default.data
+## This is the older method.  It's left here for refernce sake at this point
+## Once the code has been fully converted these lines can be deleted
+#$PYTHON import_features.py default < features/generic.features
+#$PYTHON import_features.py default < features/generic.primers
+#$PYTHON import_features.py default < features/tb.enzymes
+#$PYTHON import_features.py default < features/fp.features
+#$PYTHON create_frag_db.py default > ../frags/data/default.data
+#
+#$PYTHON import_features.py afire < features/af.unc
+#$PYTHON import_features.py afire < features/af.custom
+#$PYTHON import_features.py afire < features/af.features
+#$PYTHON import_features.py afire < features/af.enzymes
+#$PYTHON create_frag_db.py afire > ../frags/data/afire.data
+#
+#$PYTHON import_features.py none < features/all.enzymes
 
-$PYTHON import_features.py afire < features/af.unc
-$PYTHON import_features.py afire < features/af.custom
-$PYTHON import_features.py afire < features/af.features
-$PYTHON import_features.py afire < features/af.enzymes
+## Hold Current Directory
+pwd=$PWD
+
+GIRAFFE="/home/benjie/git/giraffe/src/django/giraffe"
+FIXTURE="$GIRAFFE/blat/fixtures/"
+cd $GIRAFFE
+
+#############################################################################################
+####                   Default Database Creation                                      
+echo "Importing blat/fixtures/features/generic.features to Feature_DataBase(default)"
+$PYTHON manage.py import_features --db=default --file=blat/fixtures/features/generic.features
+
+echo "Importing blat/fixtures/features/generic.primers to Feature_DataBase(default)"
+$PYTHON manage.py import_features --db=default --file=blat/fixtures/features/generic.primers
+
+echo "Importing blat/fixtures/features/tb.enzymes to Feature_DataBase(default)"
+$PYTHON manage.py import_features --db=default --file=blat/fixtures/features/tb.enzymes
+
+echo "Importing blat/fixtures/features/fp.features to Feature_DataBase(default)"
+$PYTHON manage.py import_features --db=default --file=blat/fixtures/features/fp.features
+
+cd $FIXTURE
+echo "Creating Frag DB default"
+$PYTHON create_frag_db.py default > ..//frags/data/default.data
+cd $GIRAFFE
+##############################################################################################
+
+
+##############################################################################################
+####                      aFire Database Creation
+echo "Importing blat/fixtures/features/af.unc to Feature_DataBase(afire)"
+$PYTHON manage.py import_features --db=afire --file=blat/fixtures/features/af.unc
+
+echo "Importing blat/fixtures/features/af.custom to Feature_DataBase(afire)"
+$PYTHON manage.py import_features --db=afire --file=blat/fixtures/features/af.custom
+
+echo "Importing blat/fixtures/features/af.features to Feature_DataBase(afire)"
+$PYTHON manage.py import_features --db=afire --file=blat/fixtures/features/af.features
+echo "Importing blat/fixtures/features/af.enzymes to Feature_DataBase(afire)"
+$PYTHON manage.py import_features --db=afire --file=blat/fixtures/features/af.enzymes
+
+cd $FIXTURE
+echo "Creating Frag DB afire"
 $PYTHON create_frag_db.py afire > ../frags/data/afire.data
+cd $GIRAFFE
+##############################################################################################
 
-$PYTHON import_features.py none < features/all.enzymes
+##############################################################################################
+##                    Import all.enayzmes no database
+echo "Importing blat/fixtures/features/all.enzymes to Feature_DataBase(none)"
+$PYTHON manage.py import_features --db=none --file=blat/fixtures/features/all.enzymes
+##############################################################################################
 
+## Now send the user back to were the came from
+cd $pwd
---
+++ src/django/giraffe/blat/management/commands/import_features.py
@@ -0,0 +1,236 @@
+#  import_features.py
+#  Author:  John Furr
+#  Date:  11/1/2012
+#
+#  This script is used to read feature files from 
+#  blat/fixtures/features/ 
+#  and import them into the Features Tables
+#
+#  Each Feature has a ForeignKey back to the Feature_Database
+#
+#  The management script has two required options
+#  --db=  can be 'default', 'afire', or 'none'
+#  --file=  Should be one of the feature files in blat/fixtures/features/ 
+#
+# class Feature(models.Model):
+#    type = models.ForeignKey(Feature_Type)
+#    name = models.CharField(max_length=32,db_index=True)
+#    sequence = models.TextField()
+#    hash = models.CharField(max_length=64)
+#    cut_after = models.PositiveIntegerField(null=True,blank=True)
+#    last_modified = models.DateTimeField(auto_now=True,db_index=True)
+#
+# class Feature_Database(models.Model):
+#    features = models.ManyToManyField(Feature, through='Feature_In_Database')
+#    name = models.CharField(max_length=64,unique=True)
+#    db_version = models.CharField(max_length=64)
+#    last_built = models.DateTimeField(null=True,blank=True)
+#
+#
+# class Feature_In_Database(models.Model):
+#    feature = models.ForeignKey(Feature)
+#    feature_database = models.ForeignKey(Feature_Database)
+#    show_feature = models.BooleanField(default=True)
+#
+#
+# class Feature_DB_Index(models.Model):
+#    db = models.ForeignKey(Feature_Database)
+#    feature = models.ForeignKey(Feature)
+#    feature_index = models.PositiveIntegerField(db_index=True)
+#    antisense = models.BooleanField()
+#    show_feature = models.BooleanField(default=True)
+
+## Python specific imports
+import sys
+from optparse import make_option
+import re
+import hashlib
+import datetime
+
+## Django specific imports
+from django.core.management import BaseCommand
+
+## Addgene/giraffe specific imports
+from giraffe.blat.models import Feature, Feature_Type
+from giraffe.blat.models import Feature_Database
+from giraffe.blat.models import Feature_In_Database
+
+
+class Command(BaseCommand):
+
+    help = u'''\
+        Import new Features into blat_features
+        '''
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--db',
+            action='store',
+            dest='db',
+            default='',
+            help='The Feature Database (default, afire, none)'
+        ),
+        make_option(
+            '--file',
+            action='store',
+            dest='file',
+            default='',
+            help='The Feature File  fixtures/features/*.feature'
+        ),
+    )
+
+
+    def get_feature_database(self, db_name):
+        '''
+            There are currently 3 options that we use for this process
+            default, afire, none
+        '''
+        if db_name == 'none':
+            return False
+
+        try:
+            fdb = Feature_Database.objects.get(name=db_name)
+        except:
+            print "Creating new Feature_Database: ", db_name 
+            fdb = Feature_Database()
+            fdb.name = db_name
+            fdb.db_version = hashlib.sha1(str(datetime.datetime.now())).hexdigest()
+            fdb.save()
+
+        return fdb
+
+
+    def create_enzyme_feature(self, line, fdb):
+        """ 
+            Enzyme have two possible format options
+            E:Name,1/5 Sequence
+            E*:Name,1/5 Sequence
+            
+            The 1 above is the cut_after point
+            I'd assume the 5 is the cut_before point???
+            E specifies that it's an enzyme
+            E* is a special enzyme?????  Need to finish documenting this
+
+            Example Explanation from Jason
+            E:EcoRI,1/5 GAATTC
+            1 and 5 are cut points depending upon whether or
+            not the sequence is translated via the 3' end or the 5' end
+        """
+        m = re.match('^E:(\w+),(\d)\/(\d) (.+)$',line)
+        show_feature=False
+        if not m:
+            m = re.match('^E\*:(\w+),(\d)\/(\d) (.+)$',line)
+            show_feature=True
+
+        name = m.group(1)
+        cut_after = int(m.group(2))
+        sequence = m.group(4)
+
+
+        ## Create a new feature or get the current feature from the database
+        f = Feature()
+        f.type = Feature_Type.objects.get(type='Enzyme')
+        f.name = name
+        f.sequence = sequence
+        f.cut_after = cut_after
+        try:
+            f.save()
+        except:
+            f = Feature.objects.get(name=name,hash=f.hash)
+
+        ## If the feature database was set then we 
+        ## set the feature field of this database to teh new feature
+        if fdb:
+            m = Feature_In_Database()
+            m.feature = f
+            m.feature_database = fdb
+            m.show_feature = show_feature
+            m.save() 
+    
+    def get_feature_type(self, n):
+        if n == "F":   return Feature_Type.objects.get(type='Feature')
+        if n == "G":   return Feature_Type.objects.get(type='Gene')
+        if n == "P":   return Feature_Type.objects.get(type='Promoter')
+        if n == "O":   return Feature_Type.objects.get(type='Origin')
+        if n == "R":   return Feature_Type.objects.get(type='Regulatory')
+        if n == "T":   return Feature_Type.objects.get(type='Terminator')
+        if n == "f":   return Feature_Type.objects.get(type='ExactFeature')
+        if n == "S":   return Feature_Type.objects.get(type='Primer')
+        if n == "E":   return Feature_Type.objects.get(type='Enzyme')
+        if n == "E*":  return Feature_Type.objects.get(type='Enzyme')
+
+    def create_other_feature(self, line, fdb):
+        """ 
+            ALl other features come in a standard format
+            F:Name  Sequence
+            
+            F can be, F, G, P, O, R, T, E, E*, f, S
+            See get_feature_types() above
+        """
+
+        #Don't process blank lines or comments
+        if not line.strip():  return
+        if line[0] == '#': return
+
+        # Match for F:Name Sequence
+        m = re.match('^(\w+):(\S+) (.+)$',line)
+        if not m:
+            return
+
+        n = m.group(1)
+        name = m.group(2)
+        sequence = m.group(3)
+
+        f = Feature()
+        f.type = self.get_feature_type(n)
+        f.name = name
+        f.sequence = sequence
+
+        try:
+            f.save()
+        except:
+            f = Feature.objects.get(name=name,hash=f.hash)
+
+        if fdb:
+            m = Feature_In_Database()
+            m.feature = f
+            m.feature_database = fdb
+            m.show_feature = True
+            m.save()
+ 
+ 
+    def handle(self, *args, **options):
+        ##Get or Create a new feature_database
+        ## Current options are default, afire, none
+        fdb = self.get_feature_database( options['db'] )    
+
+
+        ## Now parse the *.feautre files found in blat/fixtures/features/
+        ## There are three line format types
+        ##
+        ## 2 Enzyme Formatting options
+        ## E:Name,1/5 sequence
+        ## E*:Name,1/5 sequence
+        ##
+        ## Other feature type lines
+        ## F:Name Sequence
+        ## F can be one of the following
+        ## F: Feature
+        ## G: Gene
+        ## P: Promoter
+        ## O: Origin
+        ## R: Regulatory
+        ## T: Terminal
+        ## E: Enzyme ??
+        ## f: exact Feature
+        ## S: Primer
+        with open(options['file'], 'r') as f:
+            for line in f.readlines():
+                if line[0] == 'E':
+                    self.create_enzyme_feature(line, fdb)
+                else:
+                    self.create_other_feature(line, fdb)
+        
+
+
+
